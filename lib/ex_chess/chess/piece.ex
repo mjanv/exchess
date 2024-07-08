543457b8ec54bebcc256103a8199b531011bcd30
Maxime Janvier <maxime.janvier@gmail.com>
2024-07-09 01:17:33 +0200

    value

--- lib/ex_chess/chess/piece.ex
+++ lib/ex_chess/chess/piece.ex
@@ -7,4 +7,11 @@ defmodule ExChess.Chess.Piece do
   @type t() :: %__MODULE__{color: color(), role: role()}
 
   defstruct [:color, :role]
+  
+  def value(%__MODULE__{role: :pawn}), do: 1
+  def value(%__MODULE__{role: :knight}), do: 3
+  def value(%__MODULE__{role: :bishop}), do: 3
+  def value(%__MODULE__{role: :rook}), do: 5
+  def value(%__MODULE__{role: :queen}), do: 9
+  def value(%__MODULE__{role: :king}), do: :infinity
 end
